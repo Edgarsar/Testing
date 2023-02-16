48f4f30f8c872dfd424bd914ec54671485b6e8e0
Edgar <edgarsargsyan@hotmail.com>
2023-02-16 16:12:27 -0600

    Write a first

---
+++ jest_testing/script2.test.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+const swapi = require('./script2');
+
+
+it('calls swapi to get people', () => {
+  //expect.assertions(number) verifies that a
+  // certain number of assertions are called during a
+  // test. This is often useful when testing asynchronous
+  //code, in order to make sure that assertions in a
+  //callback actually got called.
+  expect.assertions(1)
+  return swapi.getPeople(fetch).then(data => {
+    expect(data.count).toEqual(87);
+  })
+})
