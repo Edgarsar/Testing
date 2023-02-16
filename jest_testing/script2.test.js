0f7d576901c256b90281b9b474a45498446e6cf9
Edgar <edgarsargsyan@hotmail.com>
2023-02-16 16:14:05 -0600

    Write a test for the getPeoplePromise function

--- jest_testing/script2.test.js
+++ jest_testing/script2.test.js
@@ -13,3 +13,12 @@ it('calls swapi to get people', () => {
     expect(data.count).toEqual(87);
   })
 })
+
+
+it('calls swapi to get people with promise', () => {
+  expect.assertions(2)
+  return swapi.getPeoplePromise(fetch).then(data => {
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  })
+})
